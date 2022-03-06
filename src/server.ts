7c86e9020f42df1090dbdb2373d9bce5e819fbdc
Nöl <65312133+linlogge@users.noreply.github.com>
2022-03-06 16:19:51 +0100

    install discord js

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,31 @@
-console.log("Testchen");
+import dotenv from "dotenv";
+import { Client, Intents } from "discord.js";
+
+// Inhalte der .env-Datei in Node-Prozess laden
+dotenv.config();
+
+const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+const COMMAND_PREFIX = "!";
+
+client.on("ready", () => {
+  console.log("Der Bot ist dem Server gejoint.");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content.charAt(0) !== COMMAND_PREFIX) return
+
+  const command = message.content.split(" ")[0].slice(1);
+
+  if (!command) return
+
+  if (command === "play") {
+    message.reply("Ich spiele für dich....")
+  }
+});
+
+client.login(DISCORD_BOT_TOKEN);
